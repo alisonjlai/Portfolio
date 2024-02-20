2a96e249a60f8e4ca81fbcbf9be3566b3082ce54
alisonjlai <alisonlaijy@gmail.com>
2024-02-20 01:07:45 -0800

    add files

---
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Typical from "react-typical";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://www.linkedin.com/in/laijy/">
+                <i className="fa fa-linkedin-square"></i>
+              </a>
+              <a href="https://github.com/alisonjlai">
+                <i className="fa fa-github-square"></i>
+              </a>
+              <a href="https://www.instagram.com/alisonpplai/">
+                <i className="fa fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Alison Lai</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  // loop={Infinity}
+                  steps={[
+                    "Full stack Developer 💻",
+                    2000,
+                    // "Coding Artist 🖌️",
+                    // 2000,
+                    // "Cloud Navigator ☁️",
+                    // 2000,
+                    // "Innovative Thinker 💡",
+                    // 2000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Front-End Creativity Meets Back-End Reliability: Crafting
+                Exceptional Web Applications.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire me{" "}
+            </button>
+            <a href="Jiaying Lai_Resume.pdf" download="Jiaying Lai_Resume.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
