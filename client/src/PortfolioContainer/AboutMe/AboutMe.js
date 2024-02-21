f15ed2a5fd4528533d4733c996be49678f4dc50d
alisonjlai <alisonlaijy@gmail.com>
2024-02-20 19:14:50 -0800

    Add About Me section

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,20 +1,73 @@
-import React,{useEffect} from 'react'
-import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
-import ScrollService from '../../utilities/ScrollService'
-import Animations from '../../utilities/Animations'
+import React, { useEffect } from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./AboutMe.css";
 
 export default function AboutMe(props) {
-    let fadeInScreenHandler= (screen) => {
-        if(screen.fadeScreen !== props.id)
-        return
-        Animations.animations.fadeInScreen(props.id)
-    }
-    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
-    return (
-        <div className='about-me-container screen-container' id = {props.id || ""}>
-            <div className='about-me-parent'>
-                <ScreenHeading title={'About Me'} subHeading={'What can I bring to the table?'}/>
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+  const SCREEN_CONSTANTS = {
+    description:
+      "As a driven Master's student in Computer Science at Northeastern University, I am proficient in object-oriented programming, data structures, algorithms, and practical experience in C++, React/Next with JavaScript. I am eager to join a team where I can make a tangible impact and help shape the future of technology.",
+    highlights: {
+      bullets: [
+        "Full Stack web development",
+        "Interactive Front End",
+        "React/Next.js",
+        "Building RESTful API",
+        "Managing database",
+      ],
+      heading: "Here are a Few Highlights:",
+    },
+  };
+  const renderHighlight = () => {
+    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="about-me-container screen-container" id={props.id || ""}>
+      <div className="about-me-parent">
+        <ScreenHeading
+          title={"About Me"}
+          subHeading={"What can I bring to the table?"}
+        />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
+                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+              </div>
+              {renderHighlight()}
             </div>
+            <div className="about-me-options">
+              <button className="btn primary-btn">
+                {""}
+                Hire me{" "}
+              </button>
+              <a
+                href="Jiaying Lai_Resume.pdf"
+                download="Jiaying Lai_Resume.pdf"
+              >
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
