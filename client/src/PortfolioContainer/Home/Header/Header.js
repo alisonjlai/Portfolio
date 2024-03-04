f5861bdfe9153ee01eb5a237a9f2471834177af8
alisonjlai <alisonlaijy@gmail.com>
2024-03-03 18:55:11 -0800

    remove icon

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -4,8 +4,8 @@ import {
   GET_SCREEN_INDEX,
 } from "../../../utilities/commonUtils";
 import ScrollService from "../../../utilities/ScrollService";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBars } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Header.css";
 
 export default function Header() {
